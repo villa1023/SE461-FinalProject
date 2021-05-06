b46fbb5f8763f3e3da82be0f446dae49210db53b
Oscar Villa <ovilla1023@hotmail.com>
2021-05-05 23:14:13 -0700

    First Commit

---
+++ test/org/psnbtech/BoardPanelTest.java
@@ -0,0 +1,27 @@
+package org.psnbtech;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoardPanelTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		assertEquals(4,4);
+		
+	}
+	
+	
+
+}
